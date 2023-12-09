a5e04fbcef8b04be5af5dad0ab9cb562bae9c81a
anikitin <anikitin@ocs.ru>
2023-12-09 15:14:13 +0300

    iter2: added mutex to storage map

--- internal/app/storage/storage.go
+++ internal/app/storage/storage.go
@@ -1,11 +1,14 @@
 package storage
 
+import "sync"
+
 type URLRepository interface {
 	AddURL(shortLink, originalLink string)
 	GetURL(shortLink string) (string, bool)
 }
 
 type URLStorage struct {
+	mu   sync.Mutex
 	Urls map[string]string
 }
 
@@ -16,10 +19,16 @@ func NewURLStorage() *URLStorage {
 }
 
 func (s *URLStorage) AddURL(shortLink, originalLink string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.Urls[shortLink] = originalLink
 }
 
 func (s *URLStorage) GetURL(shortLink string) (string, bool) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	url, ok := s.Urls[shortLink]
 
 	return url, ok
